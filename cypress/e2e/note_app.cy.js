1dd1bfd593bfd363d4eba52e1286a17eac1175f8
Junaid Ansari <junaidshahid.ansari@gmail.com>
2023-07-23 23:20:56 +0530

    bypassing the UI with cypress

--- cypress/e2e/note_app.cy.js
+++ cypress/e2e/note_app.cy.js
@@ -9,16 +9,16 @@ describe('Note app', function() {
   beforeEach(function() {
 
     // to reset the testing db
-    cy.request('POST', 'http://localhost:3000/api/testing/reset')
+    cy.request('POST', `${Cypress.env('BACKEND')}/testing/reset`)
     
     const user = {
       name: 'Junaid Ansari',
       username: 'junaid',
       password: 'hehe'
     }
-    cy.request('POST', 'http://localhost:3000/api/users', user)
+    cy.request('POST', `${Cypress.env('BACKEND')}/users`, user)
 
-    cy.visit('http://localhost:3000')
+    cy.visit('')
   })
 
   it('front page can be opened', function() {
@@ -34,43 +34,7 @@ describe('Note app', function() {
     cy.contains('Junaid Ansari logged in')
   })
 
-  describe('when logged in', function() {
-
-    beforeEach(function() {
-      cy.contains('Login').click()
-      cy.get('#username').type('junaid')
-      cy.get('#password').type('hehe')
-      cy.get('#login-button').click()
-    })
-
-    it('a new note can be created', function() {
-      cy.contains('New Note').click()
-      cy.get('#note-input').type('a note created by cypress')
-      cy.contains('Save').click()
-      cy.contains('a note created by cypress')
-    })
-
-    describe('and a note exists', function() {
-
-      beforeEach(function() {
-        cy.contains('New Note').click()
-        cy.get('#note-input').type('another note cypress')
-        cy.contains('Save').click()
-      })
-
-      it.only('it can be made not important', function() {
-        cy.contains('another note cypress')
-          .contains('make not important')
-          .click()
-        cy.contains('another note cypress')
-          .contains('make important')
-      })
-
-    })
-
-  })
-
-  it.only('login fails with wrong password', function() {
+  it('login fails with wrong password', function() {
     cy.contains('Login').click()
     cy.get('#username').type('junaid')
     cy.get('#password').type('wrong')
@@ -83,6 +47,43 @@ describe('Note app', function() {
       .should('have.css', 'border-style', 'solid')
 
     cy.get('html').should('not.contain', 'Junaid Ansari logged in')
+  })
+
+  describe('when logged in', function() {
+
+    beforeEach(function() {
+      cy.login({username: 'junaid', password: 'hehe'})
+    })
+
+    it('a new note can be created', function() {
+      cy.contains('New Note').click()
+      cy.get('#note-input').type('a note created by cypress')
+      cy.contains('Save').click()
+      cy.contains('a note created by cypress')
+    })
+
+    describe('and a note exists', function() {
+
+      beforeEach(function() {
+        // cy.createNote({
+        //   content: 'another note cypress',
+        //   important: true
+        // })
+        cy.contains('New Note').click()
+        cy.get('#note-input').type('another note cypress')
+        cy.contains('Save').click()
+      })
+
+      it('it can be made not important', function() {
+        cy.contains('another note cypress')
+          .contains('make not important')
+          .click()
+        cy.contains('another note cypress')
+          .contains('make important')
+      })
+
+    })
+
   })
 
 })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,27 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('login', ({username, password}) => {
+    cy.request('POST', 'http://localhost:3001/api/login', {
+        username, password
+    }).then(response => {
+      localStorage.setItem('loggedNoteappUser', JSON.stringify(response.body))
+      cy.visit('')
+    })
+})
+
+Cypress.Commands.add('createNote', ({content, important}) => {
+
+    cy.request({
+        url: 'http://localhost:3001/api/notes',
+        method: 'POST',
+        body: {content, important},
+        headers: {
+            'Auhtorization': `Bearer ${JSON.parse(localStorage.getItem('loggedNoteappUser')).token}`
+        }
+    })
+    console.log(localStorage.getItem('loggedNoteappUser'))
+    cy.visit('')
+
+})
